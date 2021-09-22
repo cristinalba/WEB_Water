d10631d252dfa5677f1ee134cd1527da586ce52c
Cristina Alba <Cristina.Jular.Alba@formandos.cinel.pt>
2021-09-22 11:32:09 +0100

    Form for new customers

--- WEB_Water/Controllers/HomeController.cs
+++ WEB_Water/Controllers/HomeController.cs
@@ -51,11 +51,11 @@ namespace WEB_Water.Controllers
 
                 mail.IsBodyHtml = true;
 
-                mail.Body = "<br/><br/><b>This customer contacted us:</b><br/>"+ 
+                mail.Body = "<br/><br/><b>This customer contacted us:</b><br/>" +
                     $"<b>Name:</b> {sendMail.Name}<br/>" +
                     $"<b>E-mail:</b> {sendMail.Email}<br/><br/>" +
-                    "<b>With the following message:</b><br/>" + 
-                    $"{sendMail.Message}<br/><br/>"+
+                    "<b>With the following message:</b><br/>" +
+                    $"{sendMail.Message}<br/><br/>" +
                     $"On {DateTime.Now}<br/>";
 
                 sc.Host = "smtp.gmail.com";
@@ -70,12 +70,53 @@ namespace WEB_Water.Controllers
                 ViewBag.Message = "Message sent!";
 
                 ModelState.Clear();
-
             }
             catch (Exception ex)
             {
                 ViewBag.Message = ex.Message.ToString();
             }
+
+            return View();
+
+        }
+        public IActionResult FormNewReader()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult FormNewReader(EmailForm sendMail)
+        {
+            SmtpClient sc = new SmtpClient();
+            MailMessage mail = new MailMessage();
+
+            mail.From = new MailAddress("kamistesta@gmail.com");
+            mail.To.Add(new MailAddress("kamistesta@gmail.com"));
+            mail.Subject = "Form from a new customer";
+
+            mail.IsBodyHtml = true;
+
+            mail.Body = "<br/><br/><b>This customer wants a new reader:</b><br/>" +
+                $"<b>Name:</b>  {sendMail.FirstName}  {sendMail.LastName} <br/>" +
+                $"<b>E-mail:</b>  {sendMail.Email}<br/><br/>" +
+                $"<b>Telephone:</b>  {sendMail.Telephone}<br/>" +
+                $"<b>NIF:</b>  {sendMail.NIF}<br/>" +
+                $"<b>Address:</b>  {sendMail.Address}<br/>" +
+                $"This form was sent on {DateTime.Now}<br/>";
+
+                sc.Host = "smtp.gmail.com";
+                sc.Port = 587;
+
+
+                sc.Credentials = new NetworkCredential("kamistesta@gmail.com", "Cinel123!");
+                sc.EnableSsl = true;
+
+                sc.Send(mail);
+
+                ViewBag.Message = "Message sent!";
+
+                ModelState.Clear();
 
             return View();
 
--- WEB_Water/Models/EmailForm.cs
+++ WEB_Water/Models/EmailForm.cs
@@ -8,6 +8,7 @@ namespace WEB_Water.Models
 {
     public class EmailForm
     {
+        //Contact Form
         [Required(ErrorMessage = "'Name' required")]
         public string Name { get; set; }
 
@@ -19,5 +20,25 @@ namespace WEB_Water.Models
 
         [Required(ErrorMessage = "'Message' required")]
         public string Message { get; set; }
+
+        //New Reader Form
+        [Required(ErrorMessage = "'First Name' required")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "'Last Name' required")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "'NIF' required")]
+        public string NIF { get; set; }
+
+        [Required(ErrorMessage = "'Telephone' required")]
+        public string Telephone { get; set; }
+
+        [Required(ErrorMessage = "'Address' required")]
+        public string Address { get; set; }
+
+
+
+
     }
 }
